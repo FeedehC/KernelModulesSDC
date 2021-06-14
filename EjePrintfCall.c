20970fdfebbb1686519788dc0c3a0fe3077ce8ef
FeedehC <feedehc@gmail.com>
2021-06-14 12:39:02 -0300

    borramos modulos viejos

--- EjePrintfCall.c
+++
@@ -1,8 +0,0 @@
-/* 	gcc -Wall -o EjePrintfCall EjePrintfCall.c 
-	strace ./EjePrintfCall						*/
-#include <stdio.h>
-int main(void)
-{ 
-	printf("hello"); 
-	return 0; 
-}
--- test1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Y va el tercero\n");
-    return 0;
-}
