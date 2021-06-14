fdb54d5166b9aad56985356ba20c32d56d23959e
FeedehC <feedehc@gmail.com>
2021-06-14 12:35:24 -0300

    drv-copy to drv

--- drv-old.c
+++ drv-old.c
@@ -1,16 +1,10 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/version.h>
 #include <linux/gpio.h>
 #include <linux/interrupt.h>
 #include <linux/timer.h>
 #include <linux/hrtimer.h>
 #include <linux/init.h>
-#include <linux/types.h>
-#include <linux/kdev_t.h>
-#include <linux/fs.h>
-#include <linux/device.h>
-#include <linux/cdev.h>
 
 #define LED1	15
 
@@ -31,44 +25,8 @@ static struct gpio buttons[] = {
 		{ 23, GPIOF_IN, "BUTTON 2" },	// turns LED off
 };
 
-//Variables usadas la configuracion de Char Dev Driver
-dev_t first = 0;
-static struct cdev c_dev; 	        // Global variable for the character device structure
-static struct class *class_dev; 	// Global variable for the device class
-
 /* Later on, the assigned IRQ numbers for the buttons are stored here */
 static int button_irqs[] = { -1, -1 };
-
-static int my_open(struct inode *i, struct file *f)
-{
-    printk(KERN_INFO "alarma: open()\n");
-    return 0;
-}
-static int my_close(struct inode *i, struct file *f)
-{
-    printk(KERN_INFO "alarma: close()\n");
-    return 0;
-}
-static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
-{
-    printk(KERN_INFO "alarma: read()\n");
-    return 0;
-}
-static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
-{
-    printk(KERN_INFO "alarma: write()\n");
-    return len;
-}
-
-static struct file_operations fops =
-{
-    // Dentro de file_operations defino las funciones que voy a implementar..!!
-    .owner = THIS_MODULE,
-    .open = my_open,
-    .release = my_close,
-    .read = my_read,
-    .write = my_write
-};
 
 /*
  * The interrupt service routine called on button presses
@@ -133,49 +91,14 @@ enum hrtimer_restart timer_callback(struct hrtimer *timer_for_restart)
 /*
  * Module init function
  */
-static int __init mod_init(void)
+static int __init gpiomode_init(void)
 {
+	int ret = 0;
     ktime_t interval;
-    int ret;
 
-    
-    /*Alocando el numero mayor*/
-    if ((alloc_chrdev_region(&first, 0, 1, "alarma")) < 0)
-    {
-        pr_err("Cannot allocate major number\n");
-        return -1;
-    }
-    pr_info("Major = %d Minor = %d \n", MAJOR(first), MINOR(first));
-
-    /*Creando estructura cdev*/
-    cdev_init(&c_dev, &fops);
-
-    /*Agregando el CDD al sistema*/
-    if ((cdev_add(&c_dev, first, 1)) < 0)
-    {
-        pr_err("Cannot add the device to the system\n");
-        goto r_class;
-    }
-
-    /*Creando estrucutra de clase*/
-    if ((class_dev = class_create(THIS_MODULE, "class_alarma")) == NULL)
-    {
-        pr_err("Cannot create the struct class\n");
-        goto r_class;
-    }
-
-    /*Creando dispositivo*/
-    if ((device_create(class_dev, NULL, first, NULL, "alarma")) == NULL)
-    {
-        pr_err("Cannot create the Device 1\n");
-        goto r_device;
-    }
-    
-    //////////////////////////////////////////////////
-    
 	printk(KERN_INFO "%s\n", __func__);
 
-    	// register LED gpios
+	// register LED gpios
 	ret = gpio_request_array(leds, ARRAY_SIZE(leds));
 
 	if (ret) {
@@ -247,10 +170,6 @@ fail2:
 
 fail1:
 	gpio_free_array(leds, ARRAY_SIZE(leds));
-r_device:
-    class_destroy(class_dev);
-r_class:
-    unregister_chrdev_region(first, 1);
 
 	return ret;
 }
@@ -258,12 +177,12 @@ r_class:
 /**
  * Module exit function
  */
-static void __exit mod_exit(void)
+static void __exit gpiomode_exit(void)
 {
 	int i;
     int ret = 0;
 
-	printk(KERN_INFO "%s\n", __func__);    
+	printk(KERN_INFO "%s\n", __func__);
 
 	// free irqs
 	free_irq(button_irqs[0], NULL);
@@ -283,18 +202,11 @@ static void __exit mod_exit(void)
 	// unregister
 	gpio_free_array(leds, ARRAY_SIZE(leds));
 	gpio_free_array(buttons, ARRAY_SIZE(buttons));
-
-    //Se elimina la configuracion de /dev
-    device_destroy(class_dev, first);
-    class_destroy(class_dev);
-    cdev_del(&c_dev);
-    unregister_chrdev_region(first, 1);
-    printk("Eliminando device driver de alarma.\n");
 }
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Maca y Fede");
 MODULE_DESCRIPTION("Modulo de Kernel para parpadear un LED y otras cositas");
 
-module_init(mod_init);
-module_exit(mod_exit);
+module_init(gpiomode_init);
+module_exit(gpiomode_exit);
