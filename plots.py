5c6a6e734ae57644c0c30c87b620b8b91b5f1b40
FeedehC <feedehc@gmail.com>
2021-06-16 16:50:15 -0300

    fede rompiste

--- plots.py
+++ plots.py
@@ -10,7 +10,7 @@ import pandas as pd
 fig = plt.figure(figsize = [30,20])
 plt.style.use('fivethirtyeight')
 
-data_skip = 20
+data_skip = 10
 
 def init_func(i):
     #fig.clear()
@@ -25,20 +25,11 @@ def init_func(i):
     plt.ylabel("LED1")
     #plt.show()
 
-def update_plot(i):
-    data_frame = pd.read_csv('data.csv')
-    axises = data_frame.to_numpy(dtype = int)
-    x = axises[:,0]
-    y = axises[:,1]
-    plt.subplot(1, 1, 1)    
-    plt.plot(y[i:i+data_skip], color="purple")
-    plt.title("Alarma")
-    plt.xlabel("Tiempo")
-    plt.ylabel("LED1")
-    #plt.plot(x[i:i+data_skip], y[i:i+data_skip], color='magenta')
-    #ax.scatter(x[i], y[i])
+# def update_plot(i):
+#     ax.plot(x[i:i+data_skip], y[i:i+data_skip], color='magenta')
+#     ax.scatter(x[i], y[i])
 
-anim = FuncAnimation(plt.gcf(), update_plot, init_func=init_func, interval=500)
+anim = FuncAnimation(plt.gcf(), init_func=init_func, interval=500)
 #anim = FuncAnimation(fig, update_plot,frames=np.arange(0, len(x), data_skip),init_func=init_func,interval=1000)
 
 plt.show()
