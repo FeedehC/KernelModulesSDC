453ade35b91a860563357744022a3e125d0e57ed
FeedehC <feedehc@gmail.com>
2021-06-16 16:47:44 -0300

    animacion casi lista

--- plots.py
+++ plots.py
@@ -10,10 +10,10 @@ import pandas as pd
 fig = plt.figure(figsize = [30,20])
 plt.style.use('fivethirtyeight')
 
-data_skip = 10
+data_skip = 20
 
 def init_func(i):
-    #ax.clear()
+    #fig.clear()
     data_frame = pd.read_csv('data.csv')
     axises = data_frame.to_numpy(dtype = int)
     x = axises[:,0]
@@ -25,8 +25,20 @@ def init_func(i):
     plt.ylabel("LED1")
     #plt.show()
 
+def update_plot(i):
+    data_frame = pd.read_csv('data.csv')
+    axises = data_frame.to_numpy(dtype = int)
+    x = axises[:,0]
+    y = axises[:,1]
+    plt.subplot(1, 1, 1)    
+    plt.plot(y[i:i+data_skip], color="purple")
+    plt.title("Alarma")
+    plt.xlabel("Tiempo")
+    plt.ylabel("LED1")
+    #plt.plot(x[i:i+data_skip], y[i:i+data_skip], color='magenta')
+    #ax.scatter(x[i], y[i])
 
-anim = FuncAnimation(plt.gcf(), init_func, interval=500)
+anim = FuncAnimation(plt.gcf(), update_plot, init_func=init_func, interval=500)
 #anim = FuncAnimation(fig, update_plot,frames=np.arange(0, len(x), data_skip),init_func=init_func,interval=1000)
 
 plt.show()
