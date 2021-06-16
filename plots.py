5ac8b9996bbdc38941ed328504a03a39bd2466b3
FeedehC <feedehc@gmail.com>
2021-06-16 16:47:40 -0300

    animacion casi lista

--- plots.py
+++ plots.py
@@ -19,15 +19,12 @@ def init_func(i):
     x = axises[:,0]
     y = axises[:,1]
     plt.subplot(1, 1, 1)    
-    plt.plot(y, color="magenta")
+    plt.plot(y, color="purple")
     plt.title("Alarma")
     plt.xlabel("Tiempo")
     plt.ylabel("LED1")
     #plt.show()
 
-# def update_plot(i):
-#     ax.plot(x[i:i+data_skip], y[i:i+data_skip], color='magenta')
-#     ax.scatter(x[i], y[i])
 
 anim = FuncAnimation(plt.gcf(), init_func, interval=500)
 #anim = FuncAnimation(fig, update_plot,frames=np.arange(0, len(x), data_skip),init_func=init_func,interval=1000)
