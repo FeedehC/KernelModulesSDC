d47851d5f2e3d11ef730a0eada70541c0d646ec9
macagauna117 <macagauna@mi.unc.edu.ar>
2021-06-16 12:20:55 -0300

    Agregado de comentarios y limpieza de codigo

--- drv.c
+++ drv.c
@@ -42,28 +42,15 @@ dev_t first = 0;
 static struct cdev c_dev; 	        // Global variable for the character device structure
 static struct class *class_dev; 	// Global variable for the device class
 
-/* Later on, the assigned IRQ numbers for the buttons are stored here */
+/* Arreglo para almacenar los numeros de las IRQ de los botones */
 static int button_irqs[] = { -1, -1 };
 
-/////////////////////////////////////
-static int __init mod_init(void);
-static void __exit mod_exit(void);
 
-static int my_open(struct inode *i, struct file *f);
-static int my_close(struct inode *i, struct file *f);
-static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off);
-static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off);
-static irqreturn_t button_isr(int irq, void *data);
-enum hrtimer_restart timer_callback(struct hrtimer *timer_for_restart);
-/////////////////////////////////////
-
-static int my_open(struct inode *i, struct file *f)
-{
+static int my_open(struct inode *i, struct file *f){
     printk(KERN_INFO "alarma: open()\n");
     return 0;
 }
-static int my_close(struct inode *i, struct file *f)
-{
+static int my_close(struct inode *i, struct file *f){
     printk(KERN_INFO "alarma: close()\n");
     return 0;
 }
@@ -71,11 +58,9 @@ static int my_close(struct inode *i, struct file *f)
 /*
 ** Esta funcion sera llamada cuando se lee el fichero de dispositivo
 */
-static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
-{
+static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off){
     printk(KERN_INFO "alarma: read()\n");
-	if (copy_to_user(buf, kernel_buffer, buf_size) > 0)
-    {
+	if (copy_to_user(buf, kernel_buffer, buf_size) > 0){
         pr_info("Error while reading data\n");
         return -1;
     }
@@ -86,59 +71,27 @@ static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off
 /*
 ** Esta funcion sera llamada cuando se escribe en el fichero de dispositivo
 */
-static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
-{
+static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off){
     printk(KERN_INFO "alarma: write()\n");
-	memset(kernelRead, '\0', buf_size);
-    if (copy_from_user(kernelRead, buf, len) > 0)
-    {
+	memset(kernelRead, '\0', buf_size);         //carga el buffer kernelRead con todos ceros
+    if (copy_from_user(kernelRead, buf, len) > 0)    {
         pr_info("Error while writing data\n");
         return len;
     }   
 
-	
-	//if(kstrtol(kernelRead, 10, led1_value) != 0)
-	if(sprintf(kernelRead, "%d", led1_value) != 0)
-	{
+	if(sprintf(kernelRead, "%d", led1_value) != 0){
 		pr_info("Error al escribir el valor de LED1.\n");
 		goto fail;
     }
 
-	/*
-    if ( kernelRead[0] == 'p' ){
-         polaridad *= -1;
-         pr_info("Data is being written, %d\n", polaridad);
-    }
-    else if( kernelRead[0] == 'u' ){ //020
-        kernelRead[0] = '0';
-        pr_info("%s\n", kernelRead);
-        if(kstrtol(kernelRead, 10, &escalaboton1) != 0)
-        {
-            pr_info("Failed converting Btn1 scale, exiting.\n");
-            goto fail;
-        }
-        pr_info("Escala boton 1 cambiada a %ld\n", escalaboton1);
-    }
-    else if( kernelRead[0] == 'd' ){
-        kernelRead[0] = '0';
-        pr_info("%s\n", kernelRead);
-        if(kstrtol(kernelRead, 10, &escalaboton2) != 0)
-        {
-            pr_info("Failed converting Btn2 scale, exiting.\n");
-            goto fail;
-        }
-        pr_info("Escala boton 2 cambiada a %ld\n", escalaboton2);
-    }*/
-
     return len;
 fail:
-    memset(kernelRead, '\0', buf_size);
+    memset(kernelRead, '\0', buf_size);         // vuelve el buffer a cero y retorna
     return len;
 }
 
-static struct file_operations fops =
-{
-    //Se definen las funciones de entrada/salida
+//Se definen las funciones de entrada/salida
+static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = my_open,
     .release = my_close,
@@ -147,11 +100,10 @@ static struct file_operations fops =
 };
 
 /*
- * The interrupt service routine called on button presses
+ * La rutina de servicio a la interrupcion cuando se presiona el boton 
  */
-static irqreturn_t button_isr(int irq, void *data)
-{
-    switch (timer_count)
+static irqreturn_t button_isr(int irq, void *data){
+    switch (timer_count)    // dependiendo de cuantas veces se presiono, varia la frecuencia del parpadeo
     {
     case 0:
         timer_interval = 3;
@@ -167,46 +119,30 @@ static irqreturn_t button_isr(int irq, void *data)
         break;
     
     default:
-        //error
         break;
     }
-
-    /*
-	if(irq == button_irqs[0]){// && !gpio_get_value(leds[0].gpio)) {
-		if(gpio_get_value(leds[0].gpio) == 1){			
-			gpio_set_value(leds[0].gpio, 0);
-		}
-		else{
-			gpio_set_value(leds[0].gpio, 1);
-		}
-	}
-	else if(irq == button_irqs[1] && gpio_get_value(leds[0].gpio)) {
-			gpio_set_value(leds[0].gpio, 0);
-	}*/
 
 	return IRQ_HANDLED;
 }
 
 /*
- * Timer function called periodically
+ * Funcion que se ejecuta cuando se dispara el timer
  */
-enum hrtimer_restart timer_callback(struct hrtimer *timer_for_restart)
-{
+enum hrtimer_restart timer_callback(struct hrtimer *timer_for_restart){
   	ktime_t currtime;
 	ktime_t interval;
 
   	currtime  = ktime_get();
-  	interval = ktime_set(timer_interval, 0);
+  	interval = ktime_set(timer_interval, 0);    //seteo cada cuando quiero que cambie el estado del led
 
-  	hrtimer_forward(timer_for_restart, currtime, interval);
+  	hrtimer_forward(timer_for_restart, currtime, interval);     //seteo nuevamente el tiempo para que se vuelva a disparar en un futuro
 
-	gpio_set_value(LED1, led1_value);
+	gpio_set_value(LED1, led1_value);       //setea el valor que va a tener el led 
 
-	memset(kernel_buffer, '\0', buf_size);
-    sprintf(kernel_buffer, "%d", led1_value);
-    //pr_info("%s\n", kernel_buffer);
+	memset(kernel_buffer, '\0', buf_size);  //cargo con ceros el kernel_buffer
+    sprintf(kernel_buffer, "%d", led1_value);   //cargo en kernel_buffer el valor que tiene el led
 
-	led1_value = !led1_value;
+	led1_value = !led1_value;               //cambia el valor del led para la proxima vez que expire el led
 
 	return HRTIMER_RESTART;
 }
@@ -216,119 +152,83 @@ enum hrtimer_restart timer_callback(struct hrtimer *timer_for_restart)
  */
 static int __init mod_init(void)
 {
-    ktime_t interval;
-    int ret;
+    ktime_t interval; 
+    int ret;        //variable para verificar errores
 
-    
+    printk(KERN_INFO "%s\n", __func__);
+ 
     /*Alocando el numero mayor*/
-    if ((alloc_chrdev_region(&first, 0, 1, "alarma")) < 0)
-    {
+    if ((alloc_chrdev_region(&first, 0, 1, "alarma")) < 0){
         pr_err("Cannot allocate major number\n");
         return -1;
     }
     pr_info("Major = %d Minor = %d \n", MAJOR(first), MINOR(first));
 
     /*Creando estructura cdev*/
-    cdev_init(&c_dev, &fops);
+    cdev_init(&c_dev, &fops);       //inicializo la estructura c_dev que es la que maneja las funciones del character device
 
-    /*Agregando el CDD al sistema*/
-    if ((cdev_add(&c_dev, first, 1)) < 0)
-    {
+    /*Agregando el CDD al sistema, Aquí, dev es la estructura cdev , num es el primer número 
+    de dispositivo al que responde este dispositivo (major) y count es la cantidad de números de dispositivo 
+    que deben asociarse con el dispositivo (minor).*/
+    if ((cdev_add(&c_dev, first, 1)) < 0){              
         pr_err("Cannot add the device to the system\n");
         goto r_class;
     }
 
-    /*Creando estrucutra de clase*/
-    if ((class_dev = class_create(THIS_MODULE, "class_alarma")) == NULL)
-    {
-        pr_err("Cannot create the struct class\n");
+    /*Creando estrucutra de clase, crea una clase de dispositivo virtual para que aparezca en /sys/class*/
+    if ((class_dev = class_create(THIS_MODULE, "class_alarma")) == NULL){   //     pr_err("Cannot create the struct class\n");
         goto r_class;
     }
 
-    /*Creando dispositivo*/
-    if ((device_create(class_dev, NULL, first, NULL, "alarma")) == NULL)
-    {
+    /*Creamos el dispositivo y lo asociamos con la clase creada anteriormente*/
+    if ((device_create(class_dev, NULL, first, NULL, "alarma")) == NULL){   
         pr_err("Cannot create the Device 1\n");
         goto r_device;
     }
-    
-    //////////////////////////////////////////////////
 
-	//Aloco memoria para el String kernel_buffer
-    if ((kernel_buffer = kmalloc(buf_size, GFP_KERNEL)) == 0)
-    {
+	//Aloco memoria para el String kernel_buffer y para el kernelRead
+    if ((kernel_buffer = kmalloc(buf_size, GFP_KERNEL)) == 0){
         pr_info(KERN_INFO "Cannot allocate the memory to the kernel\n");
         return -1;
     }
-    if ((kernelRead = kmalloc(buf_size, GFP_KERNEL)) == 0)
-    {
+    if ((kernelRead = kmalloc(buf_size, GFP_KERNEL)) == 0){
         pr_info(KERN_INFO "Cannot allocate the memory to the kernel\n");
         return -1;
     }
 
-	/////////////////////////////////////////////////
-    
-	printk(KERN_INFO "%s\n", __func__);
-
-    	// register LED gpios
-	ret = gpio_request_array(leds, ARRAY_SIZE(leds));
-
-	if (ret) {
+    //Registramos los leds
+	if (gpio_request_array(leds, ARRAY_SIZE(leds))) { 
 		printk(KERN_ERR "Unable to request GPIOs for LEDs: %d\n", ret);
-		return ret;
-	}
-
-	// register BUTTON gpios
-	ret = gpio_request_array(buttons, ARRAY_SIZE(buttons));
-
-	if (ret) {
-		printk(KERN_ERR "Unable to request GPIOs for BUTTONs: %d\n", ret);
 		goto fail1;
 	}
 
-	printk(KERN_INFO "Current button1 value: %d\n", gpio_get_value(buttons[0].gpio));
+	//Registramos los botones
+	if (gpio_request_array(buttons, ARRAY_SIZE(buttons))) {
+		printk(KERN_ERR "Unable to request GPIOs for BUTTONs: %d\n", ret);
+		goto fail2;
+	}
 
+    //Solicito IRQ asociada al boton
 	ret = gpio_to_irq(buttons[0].gpio);
 
 	if(ret < 0) {
 		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
 		goto fail2;
 	}
-
 	button_irqs[0] = ret;
-
 	printk(KERN_INFO "Successfully requested BUTTON1 IRQ # %d\n", button_irqs[0]);
 
+    //Asociamos el numero de interrupcion del boton, a la rutina de interrupcion que debe ejecutar 
 	ret = request_irq(button_irqs[0], button_isr, IRQF_TRIGGER_RISING /* | IRQF_DISABLED */, "gpiomod#button1", NULL);
-
-	if(ret) {
-		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
-		goto fail2;
-	}
-
-
-	ret = gpio_to_irq(buttons[1].gpio);
-
-	if(ret < 0) {
-		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
-		goto fail2;
-	}
-
-	button_irqs[1] = ret;
-
-	printk(KERN_INFO "Successfully requested BUTTON2 IRQ # %d\n", button_irqs[1]);
-
-	ret = request_irq(button_irqs[1], button_isr, IRQF_TRIGGER_RISING /* | IRQF_DISABLED */, "gpiomod#button2", NULL);
-
 	if(ret) {
 		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
 		goto fail3;
 	}
 
-    /* init timer, add timer function */
+    // Se configura el timer y se inicializa
 	interval = ktime_set(timer_interval, 0); //1 second, 0 nanoseconds
 	hrtimer_init(&hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-	hr_timer.function = &timer_callback;
+	hr_timer.function = &timer_callback;    //funcion que se ejecuta cuando el timer expira
 	hrtimer_start(&hr_timer, interval, HRTIMER_MODE_REL);
 
 	return 0;
@@ -353,28 +253,24 @@ r_class:
 /**
  * Module exit function
  */
-static void __exit mod_exit(void)
-{
+static void __exit mod_exit(void){
 	int i;
-    int ret = 0;
-
-	printk(KERN_INFO "%s\n", __func__);  
+    printk(KERN_INFO "%s\n", __func__);  
 
 	//Se libera la memoria del Kernel
     kfree(kernel_buffer); 
     kfree(kernelRead); 
 
-	// free irqs
+	//Se liberan las interrupciones
 	free_irq(button_irqs[0], NULL);
 	free_irq(button_irqs[1], NULL);
 
-    // check errors in hrtimer
-    ret = hrtimer_cancel(&hr_timer);
-	if(ret) {
+    //cancela un temporizador y espera a que termine el controlador
+	if(hrtimer_cancel(&hr_timer)) {
 		printk("Failed to cancel timer.\n");
 	}
 
-	// turn all LEDs off
+	//Apaga todos los leds
 	for(i = 0; i < ARRAY_SIZE(leds); i++) {
 		gpio_set_value(leds[i].gpio, 0);
 	}    
